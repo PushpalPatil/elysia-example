0a9c3f17a0535bee1cc72d0450c31d50a100b431
Pushpal Patil <gauripushp@gmail.com>
2025-06-11 14:21:26 -0400

    step 1 complete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.get("/", () => "Hello Elysia")
+.get("/api/public", "This is public information")
+.get("/api/protected", "Only admin should be able to see this")
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
